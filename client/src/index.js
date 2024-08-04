bd8c267db1dc502cee256d27e833547093786def
Prapti Bargir <praptibargir21@gmail.com>
2024-08-04 23:50:44 +0530

    signup Done

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,31 @@
 import ReactDOM from 'react-dom/client';
-import{createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './views/Home/Home';
+import Login from './views/Login/Login';
+import SignUp from './views/SignUp/SignUp';
+import "./global.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
-const router=createBrowserRouter([{
-    path:"/",
-    element:<h1>Home</h1>
-},
-{
-    path:"*",
-    element:<h1>Not Found</h1>
-}
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "/login",
+        element: <Login/>
+    },
+    {
+        path: "/signup",
+        element: <SignUp/>
+    },
+    {
+        path: "*",
+        element: <h1>Not Found</h1>
+    }
 ])
 
-root.render(<RouterProvider router={router}/>);
+root.render(<RouterProvider router={router} />);
 
--- client/src/views/SignUp/SignUp.js
+++ client/src/views/SignUp/SignUp.js
@@ -1,10 +1,95 @@
-import React from 'react'
-import "./SignUp.css"
+import React, { useState } from 'react';
+import "./SignUp.css";
+import axios from 'axios';
+import toast, { Toaster } from "react-hot-toast";
 
 function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+
+    const [user, setUser] = useState({
+        fullName: "",
+        email: "",
+        password: "",
+        dob: ""
+    })
+
+    const signup = async () => {
+        const response = await axios.post(`${process.env.REACT_APP_Backend_URL}/signup`, {
+            fullName: user.fullName,
+            email: user.email,
+            password: user.password,
+            dob: user.dob
+        })
+
+        if (response.data.success) {
+            toast.success(response.data.message)
+
+            setUser({
+                fullName: "",
+                email: "",
+                password: "",
+                dob: ""
+            })
+        } else {
+            toast.error(response.data.message)
+        }
+    }
+
+    return (
+        <div className='container'>
+            <h1 className='title'>User Registration</h1>
+
+            <form className='signup-container'>
+                <input
+                    type='text'
+                    placeholder='Enter Full Name'
+                    className='user-input'
+                    value={user.fullName}
+                    onChange={(e) => {
+                        setUser({ ...user, fullName: e.target.value })
+                    }}
+                />
+
+                <input
+                    type='email'
+                    placeholder='Enter Email'
+                    className='user-input'
+                    value={user.email}
+                    onChange={(e) => {
+                        setUser({ ...user, email: e.target.value })
+                    }}
+                />
+
+                <input
+                    type='password'
+                    placeholder='Enter Password'
+                    className='user-input'
+                    value={user.password}
+                    onChange={(e) => {
+                        setUser({ ...user, password: e.target.value })
+                    }}
+                />
+
+                <input
+                    type='date'
+                    placeholder='Date of Birth'
+                    className='user-input'
+                    value={user.dob}
+                    onChange={(e) => {
+                        setUser({ ...user, dob: e.target.value })
+                    }}
+                />
+
+
+                <button
+                    type='button'
+                    className='btn'
+                    onClick={signup}>
+                    Register Now
+                </button>
+            </form>
+            <Toaster />
+        </div>
+    )
 }
 
 export default SignUp
